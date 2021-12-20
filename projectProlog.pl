a7c162e0b32a30b963269168e0827ab047a46c11
ccyrenee <96290089+ccyrenee@users.noreply.github.com>
2021-12-20 11:51:28 +0100

    caricato id8 e car alfanumerici

--- projectProlog.pl
+++ projectProlog.pl
@@ -163,3 +163,12 @@ news(Input) :- host(Input), !.
 
 /* tel e fax */
 telfax(Input) :- userinfo(Input), !.
+
+/* caratteri alfanumerici */
+controlX(List_codes) :- member(X, List_codes), X > 47, X < 58, !.
+controlX(List_codes) :- member(X, List_codes), X > 64, X < 91, !.
+controlX(List_codes) :- member(X, List_codes), X > 96, X < 123, !.
+
+/* id8 */
+id8(Input) :- atom_codes(Input, List_codes), length(List_codes, Y), Y > 8, !, fail.
+id8(Input) :- atom_codes(Input, List_codes), controlX(List_codes), !.
