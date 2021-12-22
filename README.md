f074badff424008b851c80a7704503619b3f3c89
zully14 <giuliana.mejia12@gmail.com>
2021-12-22 07:13:47 +0100

    load

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 authority--> passa ad auto che riconosce '//' e gli ignora passa la stringa senza '//' a userinfoauto che vede se prima trova @ e se lo fa prosegua al riconoscimento di userinfo e se fallisce passa il riconosciento ad host
 (bisogna tenere in conto che stiamo passando codici ASCII dal momento che aut mi ha tolto i //)
 
-listPos--> usato per sapere posizione di un certo carattere, per esempio in authority per poter sapere dove si trova @ e :
+listPos (lista, elmento, X)--> usato per sapere posizione di un certo carattere, per esempio in authority per poter sapere dove si trova @ e :
 
 twopoints--> mi verifica dove ci sono i : se è cosi passa SubAtom a port
--- projectProlog.pl
+++ projectProlog.pl
@@ -1,7 +1,8 @@
 /* digit */
-digit(Input) :- term_string(TermInput, Input), integer(TermInput), !.
-digit255(Input) :- term_string(TermInput, Input), integer(TermInput),
+digit(Input):- term_string(TermInput, Input), !, integer(TermInput), !.
+digit255(Input):- term_string(TermInput, Input), integer(TermInput),
                    TermInput >= 0, TermInput < 256.
+
 
 /* caratteri non validi per identificatore */
 id(64). %@
@@ -10,14 +11,10 @@ id(63). %?
 id(35). %#
 id(58). %:
 
-<<<<<<< HEAD
-identificatore(Input) :- atom_codes(Input, List_input), ide(List_input).
-=======
-identificatore(Input) :- atom_codes(Input, List_input), ide(List_input), !.
->>>>>>> 622c0ffe7c37f57ba9de5d261ab3d66be5e4233b
+identificatore(Input):- atom_codes(Input, List_input), ide(List_input), !.
 
-ide([L| _]) :- id(L), ! , fail.
-ide([_ | Ls]) :- ide(Ls).
+ide([L| _]):- id(L), ! , fail.
+ide([_ | Ls]):- ide(Ls).
 ide([_]).
 
 /* caratteri non validi per identificatore_host */
@@ -28,46 +25,31 @@ idH(35). %#
 idH(58). %:
 idH(46). %.
 
-<<<<<<< HEAD
-identificatore_host(Input) :- atom_codes(Input, List_input), ids(List_input).
-=======
-identificatore_host(Input) :- atom_codes(Input, List_input), ids(List_input), !.
->>>>>>> 622c0ffe7c37f57ba9de5d261ab3d66be5e4233b
+identificatore_host(Input):- atom_codes(Input, List_input), ids(List_input), !.
 
-ids([L| _]) :- idH(L), ! , fail.
-ids([_ | Ls]) :- ids(Ls).
+ids([L| _]):- idH(L), ! , fail.
+ids([_ | Ls]):- ids(Ls).
 ids([_]).
 
 /* elimina gli spazi */
 
 elimina_spazi([], []).
-elimina_spazi([255 | Tail], Tail ) :- !.
-elimina_spazi([Head | Tail], [Head, X]) :-  elimina_spazi(Tail, X).
+elimina_spazi([255 | Tail], Tail ):- !.
+elimina_spazi([Head | Tail], [Head, X]):- elimina_spazi(Tail, X).
 
 /* posizione */
 listPos([X|_], X, 1).
-listPos([_|Tail], X, Pos) :- listPos(Tail, X, P), Pos is P + 1.
+listPos([_|Tail], X, Pos):- listPos(Tail, X, P), Pos is P + 1.
 
 /* scheme */
-scheme(Input) :- identificatore(Input), !.
+scheme(Input):- identificatore(Input), !.
 
 /* host */
-host(Input) :- identificatore_host(Input), !.
-host(Input) :- atom_codes(Input, List_codes), point(List_codes), !.
-host(Input) :- indirizzo_ip(Input), !.
+host(Input):- identificatore_host(Input), !.
+host(Input):- atom_codes(Input, List_codes), point(List_codes), !.
+host(Input):- indirizzo_ip(Input), !.
 
-<<<<<<< HEAD
-point(List_input) :- listPos(List_input, 46, Pos),
-                     atom_codes(Atom, List_input),
-                     sub_atom(Atom, 0, Pos, After, SubAtom),
-                     identificatore_host(SubAtom),
-                     length(List_input, X),
-                     Pos2 is Pos+1,
-                     C is X-Pos2,
-                     sub_atom(Atom, Pos2, C, After1, SubAtom1),
-                     identificatore_host(SubAtom1).
-=======
-point(List_codes) :- listPos(List_codes, 46, Pos),
+point(List_codes):- listPos(List_codes, 46, Pos),
                      atom_codes(Atom, List_codes),
                      P is Pos - 1,
                      sub_atom(Atom, 0, P, After, SubAtomIdH),
@@ -75,41 +57,40 @@ point(List_codes) :- listPos(List_codes, 46, Pos),
                      A is After - 1,
                      sub_atom(Atom, Pos, A, _, SubAtomIdHost),
                      identificatore_host(SubAtomIdHost).
->>>>>>> 622c0ffe7c37f57ba9de5d261ab3d66be5e4233b
 
 /* userinfo */
-userinfo(Input) :- identificatore(Input), !.
+userinfo(Input):- identificatore(Input), !.
 
 /* port */
-port(Input) :- digit255(Input), !.
+port(Input):- digit255(Input), !.
 
 /* authority */
-authority(Input) :- atom_codes(Input, List_input), c_aut(List_input).
+authority(Input):- atom_codes(Input, List_input), c_aut(List_input).
 
-c_aut([X, X | Y]) :- X == 47, member(64, Y), !, aut(Y), !.
-c_aut([X, X | Y]) :- X == 47, member(58, Y), !, twopoints(Y), !.
-c_aut([X, X | Y]) :- X == 47, atom_codes(Atom, Y), host(Atom), !.
+c_aut([X, X | Y]):- X == 47, member(64, Y), !, aut(Y), !.
+c_aut([X, X | Y]):- X == 47, member(58, Y), !, twopoints(Y), !.
+c_aut([X, X | Y]):- X == 47, atom_codes(Atom, Y), host(Atom), !.
 
-aut(List_codes):-length(List_codes, Length_list),
-            listPos(List_codes, 64, Pos),
-            P is Pos - 1,
-            atom_codes(Atom, List_codes),
-            sub_atom(Atom, 0, P, After, SubAtomAt), %stringa fino a PRIMA di @
-            A is After - 1, 
-            sub_atom(Atom, Pos, A, _ , SubAtomRest), % da @ in poi quindi vado a richiamare twopoints per vedere se ho anche present i due punti 
-            atom_codes(SubAtomRest, List_SubAtomRest),
-            twopoints(List_SubAtomRest).
+aut(List_codes):-   length(List_codes, Length_list),
+                    listPos(List_codes, 64, Pos),
+                    P is Pos - 1,
+                    atom_codes(Atom, List_codes),
+                    sub_atom(Atom, 0, P, After, SubAtomAt), %stringa fino a PRIMA di @
+                    A is After - 1, 
+                    sub_atom(Atom, Pos, A, _, SubAtomRest), % da @ in poi quindi vado a richiamare twopoints per vedere se ho anche present i due punti 
+                    atom_codes(SubAtomRest, List_SubAtomRest),
+                    twopoints(List_SubAtomRest).
 
-aut(List_codes):- length(List_codes, Length_list),
-            listPos(List_codes, 64, Pos),
-            P is Pos - 1,
-            atom_codes(Atom, List_codes),
-            sub_atom(Atom, 0, P, After, SubAtomAt), %stringa fino a PRIMA di @
-            A is After - 1, 
-            sub_atom(Atom, Pos, A, _ , SubAtomRest), % da @ in poi quindi vado a richiamare twopoints per vedere se ho anche present i due punti 
-            atom_codes(SubAtomRest, List_SubAtomRest),
-            host(SubAtomRest), !, 
-            userinfo(SubAtomAt).
+aut(List_codes):-   length(List_codes, Length_list),
+                    listPos(List_codes, 64, Pos),
+                    P is Pos - 1,
+                    atom_codes(Atom, List_codes),
+                    sub_atom(Atom, 0, P, After, SubAtomAt), %stringa fino a PRIMA di @
+                    A is After - 1, 
+                    sub_atom(Atom, Pos, A, _ , SubAtomRest), % da @ in poi quindi vado a richiamare twopoints per vedere se ho anche present i due punti 
+                    atom_codes(SubAtomRest, List_SubAtomRest),
+                    host(SubAtomRest), !, 
+                    userinfo(SubAtomAt).
 
 twopoints(List_codes):- member(58, List_codes), !,
                         length(List_codes, Length),
@@ -121,57 +102,28 @@ twopoints(List_codes):- member(58, List_codes), !,
                         sub_atom(Atom, 0,  A, _, SubAtomHost), %prende stringa host
                         host(SubAtomHost), !,
                         port(SubAtomPoints).
-<<<<<<< HEAD
-
-
-listPos([X|_], X, 1).
-listPos([_|Tail], X, Pos) :- listPos(Tail, X, P), Pos is P + 1.
-
-
 
 /* indirizzo_ip */
-indirizzo_ip(Input):- atom_codes(Input, List_input), length(List_input, 15), !, validate_point(List_input).
-=======
-
-/* indirizzo_ip */
-indirizzo_ip(Input):- atom_codes(Input, List_input),
+indirizzo_ip(Input):- atom_string(Atom, Input), atom_codes(Atom, List_input),
                       length(List_input, 15), !,
                       validate_point(List_input).
->>>>>>> 622c0ffe7c37f57ba9de5d261ab3d66be5e4233b
 
-validate_point(L):- nth1(12, L, 46), nth1(8, L, 46), nth1(4, L, 46),
+validate_point(L):- nth1(12, L, 46), nth1(8, L, 46), nth1(4, L, 46), !,
                     atom_codes(String, L),
                     split_string(String, ".", "", List_string),
                     length(List_string, 4),
                     validate_number(List_string).
 
-validate_number([L | Ls]):- digit(L), validate_number(Ls), !.
+validate_number([L | Ls]):- digit(L), !, validate_number(Ls).
 validate_number([]).
 
 /* path */
-path(Input) :- identificatore(Input), !.
-<<<<<<< HEAD
-path(Input) :- atom_codes(Input, List_input), member(47, List_input),
-               slash(List_input), !.
-
-slash(List_input) :- listPos(List_input, 47, Pos),
-                     atom_codes(Atom, List_input),
-                     sub_atom(Atom, 0, Pos, After, SubAtom),
-                     identificatore(SubAtom),
-                     length(List_input, X),
-                     Pos2 is Pos+1,
-                     C is X-Pos2,
-                     sub_atom(Atom, Pos2, C, After1, SubAtom1),
-                     identificatore(SubAtom1).
-
-/* query */
-query(Input) :- atom_codes(Input, List_input), member(35, List_input), !, fail.
-=======
-path(Input) :- atom_codes(Input, List_codes),
+path(Input):- identificatore(Input), !.
+path(Input):- atom_codes(Input, List_codes),
                member(47, List_codes),
                slash(List_codes), !.
 
-slash(List_codes) :- listPos(List_codes, 47, Pos),
+slash(List_codes):-  listPos(List_codes, 47, Pos),
                      atom_codes(Atom, List_codes),
                      P is Pos - 1,
                      sub_atom(Atom, 0, P, After, SubAtomId),
@@ -181,25 +133,24 @@ slash(List_codes) :- listPos(List_codes, 47, Pos),
                      identificatore(SubAtomIde).
 
 /* query */
-query(Input) :- atom_codes(Input, List_codes), member(35, List_codes), !, fail.
->>>>>>> 622c0ffe7c37f57ba9de5d261ab3d66be5e4233b
+query(Input):- atom_codes(Input, List_codes), member(35, List_codes), !, fail.
 query(Input).
 
 /* fragment */
 fragment(Input).
 
 /* scheme_syntax */
-scheme_syntax(Input) :- mailto(Input), !.
-scheme_syntax(Input) :- news(Input), !.
-scheme_syntax(Input) :- telfax(Input), !.
+scheme_syntax(Input):- mailto(Input), !.
+scheme_syntax(Input):- news(Input), !.
+scheme_syntax(Input):- telfax(Input), !.
 
 /* mailto */
-mailto(Input) :- userinfo(Input), !.
-mailto(Input) :- string_codes(Input, List_codes),
+mailto(Input):- userinfo(Input), !.
+mailto(Input):- atom_codes(Input, List_codes),
                  member(64, List_codes),
                  at(List_codes), !.
 
-at(List_codes) :- listPos(List_codes, 64, Pos),
+at(List_codes):- listPos(List_codes, 64, Pos),
                   atom_codes(Atom, List_codes),
                   P is Pos - 1,
                   sub_atom(Atom, 0, P, After, SubAtomAt),
@@ -209,24 +160,49 @@ at(List_codes) :- listPos(List_codes, 64, Pos),
                   host(SubAtomRest).
 
 /* news */
-news(Input) :- host(Input), !.
+news(Input):- host(Input), !.
 
 /* tel e fax */
-telfax(Input) :- userinfo(Input), !.
+telfax(Input):- userinfo(Input), !.
 
 /* caratteri alfanumerici */
-controlX(List_codes) :- member(X, List_codes), X > 47, X < 58, !.
-controlX(List_codes) :- member(X, List_codes), X > 64, X < 91, !.
-controlX(List_codes) :- member(X, List_codes), X > 96, X < 123, !.
+controlX(List_codes):- member(X, List_codes), X > 47, X < 58, !. % numeri
+controlX(List_codes):- member(X, List_codes), X > 64, X < 91, !. % lettere maiuscole
+controlX(List_codes):- member(X, List_codes), X > 96, X < 123, !. %lettere minuscole 
 
 /* id8 */
-id8(Input) :- atom_codes(Input, List_codes), length(List_codes, Y), Y > 8, !, fail.
-id8(Input) :- atom_codes(Input, List_codes), controlX(List_codes), !.
+id8(Input):- atom_codes(Input, List_codes), length(List_codes, Y), Y > 8, !, fail.
+id8(Input):- atom_codes(Input, List_codes), controlX(List_codes), !.
 
 /* id44 da controllare */
-id44(Input) :- atom_codes(Input, List_codes),
+id44(Input):- atom_codes(Input, List_codes),
                length(List_codes, Y),
                Y is 1,
                member(46, List_codes), !.
-id44(Input) :- atom_codes(Input, List_codes), length(List_codes, Y), Y > 44, !, fail.
-id44(Input) :- atom_codes(Input, List_codes), controlX(List_codes), !.
+id44(Input):- atom_codes(Input, List_codes), length(List_codes, Y), Y > 44, !, fail.
+id44(Input):- atom_codes(Input, List_codes), controlX(List_codes), !.
+
+/*zos*/
+
+zos(Input):- atom_codes(Input, List_codes), member(47, List_codes), !,
+            listPos(List_codes, 47, X).
+            Y is X + 1,
+            listPos(List_codes, 47, Y), !, 
+            sub_atom(Input, X,  )
+
+zos(Input):- atom_codes(Input, List_codes), member(28, List_codes), member(29, List_codes),
+            listPos(List_codes, 28, X), length(List_codes, Length),
+            atom_codes(Atom, List_codes),
+            L is Length - X;
+            sub_atom(Atom, 0, L, After, SubAtom),
+            id44(SubAtom), !, 
+            A is After - 2,
+            sub_atom(Atom, X, A, _,SubAtom2),
+            id8(SubAtom2).
+
+zos(Input):- id44(Input), !.
+
+
+URI(Input, X):- string_code(Input, List_codes), member(58, List_codes), !,
+                listPos()
+
